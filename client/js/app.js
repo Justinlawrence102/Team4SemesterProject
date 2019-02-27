47cf9f20b6568cd857b4f841c2ab67d371f583ad
Justinlawrence102 <justin.lawrence102@gmail.com>
2019-02-27 10:17:24 -0500

    started requestTripController

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,5 @@
 /* register the modules the application depends upon here*/
-angular.module('listings', []);
+angular.module('requests', []);
 
 /* register the application and inject all the necessary dependencies */
-var app = angular.module('directoryApp', ['listings']);
+var app = angular.module('travelAgencyApp', ['requests']);
