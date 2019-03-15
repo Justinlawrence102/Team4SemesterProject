e423c09bddf329885d3e928e04468a8262ca8bd0
Ashley <ashleyrocha@ufl.edu>
2019-03-15 15:11:06 -0400

    factory fix

--- client/js/factories/requestsFactory.js
+++ client/js/factories/requestsFactory.js
@@ -6,11 +6,11 @@ angular.module('requests', []).factory('Requests', function($http) {
 	
 	create: function(Newrequest) {
                                        console.log('submitting post from factory: '+Newrequest.firstName)
-      return $http.post('http://localhost:8080/api/requests', Newrequest)
+      return $http.post('/api/requests', Newrequest)
     },
 
     delete: function(id) {
-            return $http.delete('http://localhost:8080/api/listings/'+ id);
+            return $http.delete('/api/listings/'+ id);
 	   /**TODO
         return result of HTTP delete method
        */
