62fe5d65c2429959db1d830a6bf250dc43893906
KennethOyibo <k.oyibo@ufl.edu>
2019-03-15 16:11:49 -0400

    initialize commit

--- server/models/queries.js
+++ server/models/queries.js
@@ -1,24 +1,55 @@
 /* Fill out these functions using Mongoose queries*/
 var fs = require('fs'),
-	mongoose = require('mongoose'),
-	ClientSubmission = require('./listings.server.model.js'),
-	config = require('../config/config.js');
-	//listings = require('./listings.json');
+        mongoose = require('mongoose'),
+        ClientSubmission = require('./tripRequest.server.model.js'),
+        config = require('../config/config.js');
+        //listings = require('./listings.json');
 
 //mongoose.connect(config.db.uri, {useMongoClient: true});
 
 mongoose.connect(config.db.uri, {useMongoClient: true});
+
+mongooseDynamic.addSchemaField(client, "trips.trip1",{
+        origin: {type: String, Default:"Great"},
+        destination: {type: String, Default: "one"},
+        returnDate:{type: Date},
+        departDate:{type: Date},
+        numPeople:{type: Number},
+        notes:{type: String},
+        });
 //query to create a new listing
 
 var client1 = new ClientSubmission({
+        username: "ken1234",
+        password: "ken1234",
         firstname: "KENNETH",
         lastname: "OYIBO",
         email: "k.oyibo@ufl.edu",
-        tphone: 3522145571,
-        returnDate:3/14/2019,
-        departDate:3/14/2019,
-        numPeople:4,
-        notes:"I like apples",
+        tphone: 3520000000,
+
+        trips:{
+                trip0:{
+                        origin: "Orlando",
+                        destination: "Houston",
+                        returnDate:3/14/2019,
+                        departDate:3/14/2019,
+                        numPeople:4,
+                        notes:"I like tomatoes",
+                        created_at: new Date,
+                        updated_at: new Date   
+                },
+                trip1:{
+                        origin: "Orlando",
+                        destination: "Houston",
+                        returnDate:3/14/2019,
+                        departDate:3/14/2019,
+                        numPeople:4,
+                        notes:"I like apples",
+                        created_at: new Date,
+                        updated_at: new Date 
+                }
+
+        },
         created_at: new Date,
         updated_at: new Date
 
@@ -29,4 +60,3 @@ client1.save(function(err){
 
         if (err) throw(err);
 });
-
