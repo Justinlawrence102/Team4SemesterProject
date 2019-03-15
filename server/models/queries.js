ee828ebfb7c68d4b71964eb099aef806a635a8ed
KennethOyibo <k.oyibo@ufl.edu>
2019-03-15 16:24:41 -0400

    initialize commit

--- server/models/queries.js
+++ server/models/queries.js
@@ -1,8 +1,10 @@
 /* Fill out these functions using Mongoose queries*/
 var fs = require('fs'),
         mongoose = require('mongoose'),
-        ClientSubmission = require('./tripRequest.server.model.js'),
+        client = require('./tripRequest.server.model.js'),
+	mongooseDynamic = require ('mongoose-dynamic-schemas');
         config = require('../config/config.js');
+
         //listings = require('./listings.json');
 
 //mongoose.connect(config.db.uri, {useMongoClient: true});
@@ -19,7 +21,7 @@ mongooseDynamic.addSchemaField(client, "trips.trip1",{
         });
 //query to create a new listing
 
-var client1 = new ClientSubmission({
+var client1 = new client({
         username: "ken1234",
         password: "ken1234",
         firstname: "KENNETH",
