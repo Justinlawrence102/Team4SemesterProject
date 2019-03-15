d31a44a2c0a189f772177910fad944a20ff50e25
KennethOyibo <k.oyibo@ufl.edu>
2019-03-15 15:04:48 -0400

    initialize commit

--- server/models/listings.server.model.js
+++ server/models/listings.server.model.js
@@ -3,24 +3,35 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
-var clientSubmissionSchema = new Schema({
+
+var clientSchema = new Schema({
+
+	username: {type: String , unique: true,required: true},
+	password: {type: String, unique: true, required: true},
 	firstname: {type: String, required: true, index: true},
 	lastname: {type: String, required: true, index: true},
-	email: {type: String, required: true},
-	tphone: {type: Number, required:true},
-	returnDate:{type: Date, required:true},
-	departDate:{type: Date, required:true},
-	numPeople:{type: Number, required:true},
-	notes:{type: String},
+	email: {type: String, required: true, required: true},
+	tphone: {type: Number},
+	tripCount: {type:Number, default: 0},
+	trips:{
+		trip0:{
+			origin: {type: String},
+			destination: {type: String},
+			returnDate:{type: Date},
+			departDate:{type: Date},
+			numPeople:{type: Number},
+			notes:{type: String},
+		},
+	},
 	created_at: Date,
 	updated_at: Date
+},{ collection: 'users' });
 
-});
 
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 
-clientSubmissionSchema.pre('save', function(next) {
+clientSchema.pre('save', function(next) {
   var currentTime = new Date;
   this.updated_at = currentTime;
   if(!this.created_at)
@@ -30,9 +41,16 @@ clientSubmissionSchema.pre('save', function(next) {
   next();
 });
 
+//This is a function to get the full name. It does not persist to the DB
+//get fullname by making the call "var.fullName"
+clientSchema.virtual('fullName').get(function () {
+  return this.firstname + ' ' + this.lastname;
+});
+
 
 /* Use your schema to instantiate a Mongoose model */
-var ClientSubmission = mongoose.model('ClientSubmission', clientSubmissionSchema);
+var client = mongoose.model('clients', clientSchema)
 
 /* Export the model to make it avaiable to other parts of your Node application */
-module.exports = ClientSubmission;
+
+module.exports = client;
--- server/models/queries.js
+++ server/models/queries.js
@@ -1,24 +1,49 @@
 /* Fill out these functions using Mongoose queries*/
 var fs = require('fs'),
 	mongoose = require('mongoose'),
-	ClientSubmission = require('./listings.server.model.js'),
+	client = require('./listings.server.model.js'),
+        mongooseDynamic = require ('mongoose-dynamic-schemas'),
 	config = require('../config/config.js');
 	//listings = require('./listings.json');
-
-//mongoose.connect(config.db.uri, {useMongoClient: true});
 
 mongoose.connect(config.db.uri, {useMongoClient: true});
 //query to create a new listing
 
-var client1 = new ClientSubmission({
+mongooseDynamic.addSchemaField(client, "trips.trip1",{
+                        origin: {type: String, Default:"Great"},
+                        destination: {type: String, Default: "one"},
+                        returnDate:{type: Date},
+                        departDate:{type: Date},
+                        numPeople:{type: Number},
+                        notes:{type: String},
+                });
+
+var client1 = new client({
+        username: "ken1234",
+        password: "ken1234",
         firstname: "KENNETH",
         lastname: "OYIBO",
         email: "k.oyibo@ufl.edu",
-        tphone: 3522145571,
-        returnDate:3/14/2019,
-        departDate:3/14/2019,
-        numPeople:4,
-        notes:"I like apples",
+        tphone: 3520000000,
+        trips:{
+                trip0:{
+                        origin: "Orlando",
+                        destination: "Houston",
+                        returnDate:3/14/2019,
+                        departDate:3/14/2019,
+                        numPeople:4,
+                        notes:"I like tomatoes"   
+                },
+                trip1:{
+                        origin: "Orlando",
+                        destination: "Houston",
+                        returnDate:3/14/2019,
+                        departDate:3/14/2019,
+                        numPeople:4,
+                        notes:"I like apples"   
+                }
+
+        },
         created_at: new Date,
         updated_at: new Date
 
