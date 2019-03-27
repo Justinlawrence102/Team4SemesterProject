4708185c65f8b950c518130b48e51439ee8555f6
Justinlawrence102 <justin.lawrence102@gmail.com>
2019-03-27 15:44:52 -0400

    removed duplicate code to get all users (replaced api/users with api/clients

--- client/js/controllers/adminDashboardController.js
+++ client/js/controllers/adminDashboardController.js
@@ -1,14 +1,13 @@
-angular.module('users').controller('adminDashboardController', ['$scope', 'Requests',  function($scope, Requests) {
-                                                                $scope.userName = undefined;
-                                                                $scope.firstName = undefined;
-                                                                $scope.lastName = undefined;
+angular.module('clients').controller('adminDashboardController', ['$scope', 'Requests',  function($scope, Requests) {
+         $scope.userName = undefined;
+         $scope.firstName = undefined;
+         $scope.lastName = undefined;
                                                                 
-                                                                console.log('inside controllerx')
-                                                                Requests.getAllClients().then(function(response) {
-                                                                                              console.log('trying to get users')
-                                                                                              $scope.userRequest = response.data;
-                                                                                              }, function(error) {
-                                                                                              console.log('Unable to retrieve list of users:', error);
-                                                                                              });
-                                                                }
+    Requests.getAllClients().then(function(response) {
+console.log('trying to get users')
+    $scope.userRequest = response.data;
+     }, function(error) {
+         console.log('Unable to retrieve list of users:', error);
+    });
+   }
                                                                 ]);
