7865623ec699ca7a9459d4a81ff4d59549b3878a
Ashley <ashleyrocha@ufl.edu>
2019-04-09 00:21:26 -0400

    contact page emailing

--- server/config/express.js
+++ server/config/express.js
@@ -10,7 +10,10 @@
     tripRequestRouter = require('../routes/tripRequest.server.routes.js'),
     clientRouter = require('../routes/client.server.routes.js'),
     indexRouter = require('../routes/index.routes.js'),
-    recomendationsRouter = require('../routes/adminDashboard.server.routes.js');
+    recomendationsRouter = require('../routes/adminDashboard.server.routes.js'),
+
+    nodemailer = require("nodemailer"),
+    flash = require('connect-flash');
 
 
 module.exports.init = function() {
@@ -24,6 +27,14 @@ module.exports.init = function() {
   //initialize app
   var app = express();
   var authRouter = require('../routes/auth.server.routes.js')(passport);
+  var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+      user: "bochtraveltest",
+      pass: "thisisatest1234"
+    }
+  })
 
   //enable request logging for development debugging
   app.use(morgan('dev'));
@@ -33,7 +44,6 @@ module.exports.init = function() {
 
   //body parsing middleware 
   app.use(bodyParser.json());
-
   
   /**TODO
   Serve static files */
@@ -41,7 +51,6 @@ module.exports.init = function() {
     
   console.log("dirset it "+__dirname);
   app.use(express.static('client'));
-
 
   //initialize passport
   app.use(passport.initialize());
@@ -54,6 +63,15 @@ module.exports.init = function() {
     resave: false
   }))
 
+
+  //flash messages --testing
+  app.use(flash());
+  app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+  });
+
+
   /**TODO 
   Use the listings router for requests to the api */
     app.use('/', indexRouter);
@@ -61,11 +79,33 @@ module.exports.init = function() {
     app.use('/api/clients', clientRouter);
     app.use('/api/recomendations', recomendationsRouter);
     app.use('/api/users',recomendationsRouter);
-
-    //ash
     app.use('/api/auth', authRouter);
-  
 
+    app.get('/send', function (req,res) {
+      var mailOptions = {
+        from: req.query.to,
+        to: "bochtraveltest@gmail.com",
+        subject: req.query.subject + " [Email sent from " + req.query.name + ", " + req.query.to + "].",
+        text: req.query.text + "\n\nEmail sent from:" + "\nName: " + req.query.name + "\nEmail address: " + req.query.to
+      }
+      console.log(mailOptions);
+      res.type("text/plain");
+
+      smtpTransport.sendMail(mailOptions, function(error,response) {
+        if (error) {
+          console.log(error);
+          res.end("error");
+        }
+        else {
+          res.end("sent");
+        }
+      });
+    });
+
+    app.get('/contact-success', function (req,res) {
+      res.sendfile('./client/contact-success.html');
+    });
+  
   /**TODO
   Go to homepage for all routes not specified */
    // app.get('/', function(req, res){ 
